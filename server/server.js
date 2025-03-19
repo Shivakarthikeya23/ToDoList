4b05d01b1cd50394a7e2a32b93115ddbe5eb4f02
shiva karthikeya <shivakarthikeya5@gmail.com>
2025-03-19 15:26:30 -0400

    Deployment fixes

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 // CORS configuration
 app.use(
   cors({
-    origin: true, // Allow all origins during development
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
